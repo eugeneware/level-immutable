35f984f5ef365316275ff93208621dbb26060a5f
Eugene Ware <eugene@noblesamurai.com>
2014-01-14 00:27:08 +1100

    mocha -> tape

--- test/index.js
+++ test/index.js
@@ -1,144 +1,142 @@
-var expect = require('expect.js'),
-    level = require('level'),
+var level = require('level'),
     bytewise = require('bytewise'),
     path = require('path'),
     range = require('range'),
     rimraf = require('rimraf'),
+    redtape = require('redtape'),
     immutable = require('..');
 
-describe('level-immutable', function() {
-  var db;
+function beforeEach(done) {
+  var dbPath = path.join(__dirname, '..', 'data', 'test');
+  rimraf.sync(dbPath);
+  var db = immutable(level(dbPath, { keyEncoding: bytewise, valueEncoding: 'json' }));
+  done(null, db);
+}
 
-  beforeEach(function(done) {
-    var dbPath = path.join(__dirname, '..', 'data', 'test');
-    rimraf.sync(dbPath);
-    db = immutable(level(dbPath, { keyEncoding: bytewise, valueEncoding: 'json' }));
-    done();
+function afterEach(db, done) {
+  db.close(done);
+}
+
+var it = redtape(beforeEach, afterEach);
+
+it('should be able to store some data in the database', function(t, db) {
+  db.immutable.put('eugene', { name: 'Eugene', color: 'blue' }, get);
+
+  function get(err) {
+    t.error(err);
+    db.immutable.get('eugene', check);
+  }
+
+  function check(err, data) {
+    t.error(err);
+    t.deepEqual(data, { name: 'Eugene', color: 'blue' });
+    t.end();
+  }
+});
+
+it('should be able to store some data and change it', function(t, db) {
+  db.immutable.put('eugene', { name: 'Eugene', color: 'blue' }, delay);
+
+  function delay(err) {
+    t.error(err);
+    setTimeout(put, 5);
+  }
+
+  function put() {
+    db.immutable.put('eugene', { name: 'Eugene', color: 'black' }, get);
+  }
+
+  function get(err) {
+    t.error(err);
+    db.immutable.get('eugene', check);
+  }
+
+  function check(err, data) {
+    t.error(err);
+    t.deepEqual(data, { name: 'Eugene', color: 'black' });
+    t.end();
+  }
+});
+
+it('should be able to delete some data', function(t, db) {
+  db.immutable.put('eugene', { name: 'Eugene', color: 'blue' }, delay);
+
+  function delay(err) {
+    t.error(err);
+    setTimeout(del, 5);
+  }
+
+  function del() {
+    db.immutable.del('eugene', get);
+  }
+
+  function get(err) {
+    t.error(err);
+    db.immutable.get('eugene', check);
+  }
+
+  function check(err, data) {
+    t.equal(err.name, 'NotFoundError');
+    t.equal(data, undefined);
+    t.end();
+  }
+});
+
+it('should be able to batch data', function(t, db) {
+  var data = range(0, 10).map(function (i) {
+    return {
+      type: 'put',
+      key:  'name ' + i,
+      value: { number: 10*i }
+    };
   });
+  db.immutable.batch(data, get);
 
-  afterEach(function(done) {
-    db.close(done);
-  });
+  function get(err) {
+    t.error(err);
+    db.immutable.get('name 3', check);
+  }
 
-  it('should be able to store some data in the database', function(done) {
-    db.immutable.put('eugene', { name: 'Eugene', color: 'blue' }, get);
+  function check(err, data) {
+    t.error(err);
+    t.deepEqual(data, { number: 30 });
+    t.end();
+  }
+});
 
-    function get(err) {
-      if (err) return done(err);
-      db.immutable.get('eugene', check);
-    }
+it('should be able to access a data snapshot in the past', function(t, db) {
+  var cmds = [ put, update, del ];
 
-    function check(err, data) {
-      if (err) return done(err);
-      expect(data).to.eql({ name: 'Eugene', color: 'blue' });
-      done();
-    }
-  });
+  function put(cb) {
+    db.immutable.put('eugene', { name: 'Eugene', color: 'blue' }, cb);
+  }
 
-  it('should be able to store some data and change it', function(done) {
-    db.immutable.put('eugene', { name: 'Eugene', color: 'blue' }, delay);
+  function update(cb) {
+    db.immutable.put('eugene', { name: 'Eugene', color: 'black' }, cb);
+  }
 
-    function delay(err) {
-      if (err) return done(err);
-      setTimeout(put, 5);
-    }
+  function del(cb) {
+    db.immutable.del('eugene', cb);
+  }
 
-    function put() {
-      db.immutable.put('eugene', { name: 'Eugene', color: 'black' }, get);
-    }
-
-    function get(err) {
-      if (err) return done(err);
-      db.immutable.get('eugene', check);
-    }
-
-    function check(err, data) {
-      if (err) return done(err);
-      expect(data).to.eql({ name: 'Eugene', color: 'black' });
-      done();
-    }
-  });
-
-  it('should be able to delete some data', function(done) {
-    db.immutable.put('eugene', { name: 'Eugene', color: 'blue' }, delay);
-
-    function delay(err) {
-      if (err) return done(err);
-      setTimeout(del, 5);
-    }
-
-    function del() {
-      db.immutable.del('eugene', get);
-    }
-
-    function get(err) {
-      if (err) return done(err);
-      db.immutable.get('eugene', check);
-    }
-
-    function check(err, data) {
-      expect(err.name).to.equal('NotFoundError');
-      expect(data).to.equal(undefined);
-      done();
-    }
-  });
-
-  it('should be able to batch data', function(done) {
-    var data = range(0, 10).map(function (i) {
-      return {
-        type: 'put',
-        key:  'name ' + i,
-        value: { number: 10*i }
-      };
+  var times = [], i = 0;
+  (function next() {
+    if (cmds.length === 0) return get();
+    var cmd = cmds.shift();
+    cmd(function (err) {
+      t.error(err);
+      times[i++] = Date.now();
+      setTimeout(next, 5);
     });
-    db.immutable.batch(data, get);
+  })();
 
-    function get(err) {
-      if (err) return done(err);
-      db.immutable.get('name 3', check);
-    }
+  function get() {
+    db.immutable.get('eugene', { toTime: times[1] }, check);
+  }
 
-    function check(err, data) {
-      if (err) return done(err);
-      expect(data).to.eql({ number: 30 });
-      done();
-    }
-  });
-
-  it('should be able to access a data snapshot in the past', function(done) {
-    var cmds = [ put, update, del ];
-
-    function put(cb) {
-      db.immutable.put('eugene', { name: 'Eugene', color: 'blue' }, cb);
-    }
-
-    function update(cb) {
-      db.immutable.put('eugene', { name: 'Eugene', color: 'black' }, cb);
-    }
-
-    function del(cb) {
-      db.immutable.del('eugene', cb);
-    }
-
-    var times = [], i = 0;
-    (function next() {
-      if (cmds.length === 0) return get();
-      var cmd = cmds.shift();
-      cmd(function (err) {
-        if (err) return done(err);
-        times[i++] = Date.now();
-        setTimeout(next, 5);
-      });
-    })();
-
-    function get() {
-      db.immutable.get('eugene', { toTime: times[1] }, check);
-    }
-
-    function check(err, data) {
-      if (err) return done(err);
-      expect(data).to.eql({ name: 'Eugene', color: 'black' });
-      done();
-    }
-  });
+  function check(err, data) {
+    t.error(err);
+    t.deepEqual(data, { name: 'Eugene', color: 'black' });
+    t.end();
+  }
 });
